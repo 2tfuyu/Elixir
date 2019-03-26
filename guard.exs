eb23add75c3755360524f44ac9ecbe0027cd0bfb
2tf <39296516+nitf@users.noreply.github.com>
2019-03-26 23:43:57 +0900

    syntax

--- guard.exs
+++ guard.exs
@@ -1,13 +1,13 @@
 defmodule Guard do
-    def this_is x when is_number x do
+    def this_is(x) when is_number(x) do
         IO.puts "{x} is a number"
     end
 
-    def this_is x when is_list x do
+    def this_is(x) when is_list(x) do
         IO.puts "{x} is a list"
     end
 
-    def this_is x when is_atom x do
+    def this_is(x) when is_atom(x) do
         IO.puts "{x} is an atom"
     end
 end
