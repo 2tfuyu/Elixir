94fa7933855a5aecf4fc74ccaed11394739e38a1
Takaki <2tf2003@gmail.com>
2019-05-27 18:37:20 +0900

    Update FizzBuzz.ex

--- FizzBuzz.ex
+++ FizzBuzz.ex
@@ -1,4 +1,10 @@
 defmodule FizzBuzz do
+  # def start(max) when max > 0, do: 1..max |> Enum.map(fn(x) -> fizzbuzz(x, rem(x, 3), rem(x, 5)) end)
+
+  # def start(max) when max > 0, do: 1..max |> Enum.map(&(fizzbuzz(&1, rem(&1, 3), rem(&1, 5))))
+
+  # def start(max) when max > 0, do: Enum.map(1..max, &process / 1)
+
   def start(max) when max > 0, do: 1..max |> Enum.map(&process / 1)
 
   defp process(n), do: fizzbuzz(n, rem(n, 3), rem(n, 5))
@@ -9,4 +15,4 @@ defmodule FizzBuzz do
   defp fizzbuzz(n, _, _), do: n |> Integer.to_string |> IO.puts
 end
 
-# 100 |> FizzBuzz.start
+100 |> FizzBuzz.start
